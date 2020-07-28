4f7d87a5cddb89c180038cb058a76c94cb71dea5
Kurt Lourens <hi@kurtlourens.com>
2020-07-28 16:41:36 +0200

    fix route

--- AssistantHytale.Api/Controllers/ContactController.cs
+++ AssistantHytale.Api/Controllers/ContactController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AssistantHytale.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ContactController : ControllerBase
     {
