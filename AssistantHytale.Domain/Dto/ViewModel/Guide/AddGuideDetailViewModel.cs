352f49e5f0aa99312c1aa2331e9aa6949c3e77c6
Kurt Lourens <hi@kurtlourens.com>
2020-08-31 16:53:57 +0200

    rename

---
+++ AssistantHytale.Domain/Dto/ViewModel/Guide/AddGuideDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssistantHytale.Domain.Dto.ViewModel.Guide
+{
+    public class AddGuideDetailViewModel
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public bool ShowCreatedByUser { get; set; }
+        public string LanguageCode { get; set; }
+        public int Minutes { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
