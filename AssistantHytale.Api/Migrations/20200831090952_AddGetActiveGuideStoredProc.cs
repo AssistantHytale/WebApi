d5025ebd990cbfd139c3ed49176c76d99fd366b3
Kurt Lourens <hi@kurtlourens.com>
2020-08-31 11:47:33 +0200

    allow adding of sql files to persistence and then kick off migrations from them

---
+++ AssistantHytale.Api/Migrations/20200831090952_AddGetActiveGuideStoredProc.cs
@@ -0,0 +1,21 @@
+﻿using AssistantHytale.Persistence.Helper;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AssistantHytale.Api.Migrations
+{
+    public partial class AddGetActiveGuideStoredProc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string guideSchemaSql = SqlFileHelper.GetSqlFileContentFromStoredProceduresFolder("CreateGuideSchema.sql");
+            migrationBuilder.Sql(guideSchemaSql);
+            string sql = SqlFileHelper.GetSqlFileContentFromStoredProceduresFolder("GetActiveGuidesPaged.sql");
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ AssistantHytale.Persistence/Helper/SqlFileHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace AssistantHytale.Persistence.Helper
+{
+    public class SqlFileHelper
+    {
+        public static string GetSqlFileContentFromStoredProceduresFolder(string filename)
+        {
+            string projectDir = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+#if DEBUG
+            projectDir = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "AssistantHytale.Persistence");
+#endif
+            string persistenceStoredProcDir = Path.Combine(projectDir, "StoredProcedures");
+            string sqlFile = Path.Combine(persistenceStoredProcDir, filename);
+            return File.ReadAllText(sqlFile);
+        }
+    }
+}
